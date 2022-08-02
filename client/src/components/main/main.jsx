b5afb8fb826dc6f2d96b087ea3490e8e219c6ebf
Andrés Salazar <andrespjm@gmail.com>
2022-08-02 12:22:15 -0500

    finished order by name and rating | frontend

--- client/src/components/main/main.jsx
+++ client/src/components/main/main.jsx
@@ -4,10 +4,42 @@ import { Loader } from '../loader/Loader';
 import { Paginator } from '../paginator/Paginator';
 import styles from './main.module.css';
 
-export const Main = ({ videogames, prevPage, nextPage }) => {
+export const Main = ({ videogames, prevPage, nextPage, setSort }) => {
+	const handleChange = e => {
+		const { value } = e.target;
+		setSort(value);
+	};
+
 	return (
 		<article className='main'>
-			<h1 className={styles.sectioName}>Trending now</h1>
+			<div className={styles.optionsTop}>
+				<h1 className={styles.sectioName}>Trending now</h1>
+				<div className={styles.sort}>
+					Sort by:&nbsp;
+					<select onChange={handleChange}>
+						<option key='name' hidden value='name'>
+							Name
+						</option>
+						<option key='az' value='az'>
+							A-Z
+						</option>
+						<option key='za' value='za'>
+							Z-A
+						</option>
+					</select>
+					<select onChange={handleChange}>
+						<option key='rating' hidden value='rating'>
+							Rating
+						</option>
+						<option key='highest' value='highest'>
+							Highest
+						</option>
+						<option key='lowest' value='lowest'>
+							Lowest
+						</option>
+					</select>
+				</div>
+			</div>
 			{!videogames().length && <Loader />}
 			<section className={styles.content}>
 				{videogames().map(videogame =>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -13,6 +13,8 @@ export const HomePage = () => {
 	const [currentPage, setCurrentPage] = useState(0);
 	const [genreName, setGenreName] = useState('');
 	const [inputValue, setInputValue] = useState('');
+	const [sort, setSort] = useState('');
+	// const [sortRating, setSortRating] = useState('');
 
 	const genres = useSelector(state => state.genres);
 	const allVideogames = useSelector(state => state.videogames);
@@ -24,21 +26,43 @@ export const HomePage = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	function SortArrayName(x, y) {
+		return x.name.localeCompare(y.name);
+	}
+
+	function SortArrayRating(x, y) {
+		return x.rating - y.rating;
+	}
+
 	const filteredVideogames = () => {
 		if (genreName) {
 			const gamesPerGenre = allVideogames.filter(game =>
 				game.genres.find(genre => genre.name === genreName)
 			);
 
-			// return paginator(gamesPerGenre);
 			return gamesPerGenre;
 		}
 		// return paginator(videogames);
 		return allVideogames;
 	};
 
-	const videogames = () =>
-		filteredVideogames().slice(currentPage, currentPage + VIDEOGAMES_PER_PAGE);
+	const videogames = () => {
+		const videogames = filteredVideogames().slice(
+			currentPage,
+			currentPage + VIDEOGAMES_PER_PAGE
+		);
+		if (sort === 'az') {
+			return videogames.sort(SortArrayName);
+		} else if (sort === 'za') {
+			return videogames.sort(SortArrayName).reverse();
+		} else if (sort === 'highest') {
+			return videogames.sort(SortArrayRating).reverse();
+		} else if (sort === 'lowest') {
+			return videogames.sort(SortArrayRating);
+		} else {
+			return videogames;
+		}
+	};
 
 	const prevPage = () => {
 		if (currentPage > 0) setCurrentPage(currentPage - VIDEOGAMES_PER_PAGE);
@@ -62,7 +86,12 @@ export const HomePage = () => {
 				setCurrentPage={setCurrentPage}
 				setInputValue={setInputValue}
 			/>
-			<Main videogames={videogames} prevPage={prevPage} nextPage={nextPage} />
+			<Main
+				videogames={videogames}
+				prevPage={prevPage}
+				nextPage={nextPage}
+				setSort={setSort}
+			/>
 			<Footer />
 		</main>
 	);
